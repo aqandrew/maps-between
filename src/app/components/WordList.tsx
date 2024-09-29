ea718966c87f9a1f6c18e6b6d902f66789a38912
Andrew Aquino <dawneraq@gmail.com>
2024-09-29 16:38:13 -0700

    refactor: render WordList button as dynamic tag

--- src/app/components/WordList.tsx
+++ src/app/components/WordList.tsx
@@ -8,30 +8,19 @@ interface WordListProps {
 }
 
 export default function WordList({ choices, setWord }: WordListProps) {
-	const isCategories = choices === CATEGORIES;
+	const Button = choices === CATEGORIES ? 'button' : Dialog.Close;
 
 	return (
 		<div className="h-80 my-3 flex flex-col items-center gap-2 overflow-y-auto">
-			{choices.map((choice, i) => {
-				// TODO is there a way to DRY up this return without drilling setIsMessagesModalOpen as a prop?
-				return isCategories ? (
-					<button
-						onClick={() => setWord(choice)}
-						className="w-60 bg-gray-200"
-						key={i}
-					>
-						{choice}
-					</button>
-				) : (
-					<Dialog.Close
-						onClick={() => setWord(choice)}
-						className="w-60 bg-gray-200"
-						key={i}
-					>
-						{choice}
-					</Dialog.Close>
-				);
-			})}
+			{choices.map((choice, i) => (
+				<Button
+					onClick={() => setWord(choice)}
+					className="w-60 bg-gray-200"
+					key={i}
+				>
+					{choice}
+				</Button>
+			))}
 		</div>
 	);
 }
