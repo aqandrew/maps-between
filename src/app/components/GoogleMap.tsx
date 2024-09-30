55ee310036a9c64e06659f31caa599c958c98a43
Andrew Aquino <dawneraq@gmail.com>
2024-09-29 19:11:23 -0700

    feat: place sample marker

--- src/app/components/GoogleMap.tsx
+++ src/app/components/GoogleMap.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+
+const chargingBullLocation: google.maps.LatLngLiteral = {
+	lat: 40.705576,
+	lng: -74.013421,
+};
 
 export default function GoogleMap() {
 	return (
@@ -8,10 +13,17 @@ export default function GoogleMap() {
 			<Map
 				style={{ width: '100vw', height: '100vh' }}
 				defaultCenter={{ lat: 22.54992, lng: 0 }}
+				mapId={'3a9db3895a3964fd'}
 				defaultZoom={3}
 				gestureHandling={'greedy'}
 				disableDefaultUI={false}
-			/>
+			>
+				<AdvancedMarker
+					position={chargingBullLocation}
+					clickable={true}
+					onClick={() => console.log('marker clicked')}
+				/>
+			</Map>
 		</APIProvider>
 	);
 }
