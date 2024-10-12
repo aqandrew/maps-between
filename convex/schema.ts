afc4871d6b0eeaa35921190243bde4e16763006d
Andrew Aquino <dawneraq@gmail.com>
2024-10-12 14:16:17 -0700

    chore: add pov to message schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,6 +7,10 @@ export default defineSchema({
 			lat: v.float64(),
 			lng: v.float64(),
 		}),
+		pov: v.object({
+			lat: v.float64(),
+			lng: v.float64(),
+		}),
 		message: v.string(),
 		userId: v.string(),
 	}),
